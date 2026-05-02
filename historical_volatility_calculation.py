b5fa045b1f49fd5b681ed29b43f6f8eab7006326
niranjangowdakm423-gif <niranjangowdakm423@gmail.com>
2026-05-02 12:13:19 +0530

    Add files via upload

---
+++ historical_volatility_calculation.py
@@ -0,0 +1,16 @@
+def historical_volatility(name='^NSEI',period_='1y'):
+  import yfinance as yf
+  import numpy as np
+  import math as m
+  data = yf.download(name, period=period_)
+  closes = data['Close'][name].dropna().tolist()
+  log_returns=[]
+  for i in range(1,len(closes)):
+      r=m.log(closes[i]/closes[i-1])
+      log_returns.append(r)
+  sigma_daily=np.std(log_returns,ddof=1)
+  sigma_annually=sigma_daily*(252)**(0.5)
+  return sigma_annually
+print(historical_volatility())
+
+
